9f6c7cddd264de0d9c683a19b7cca66de0945482
JM-GL <jm.sk9@hotmail.com>
2020-09-23 21:50:46 -0300

    faq ok e tela de pedidos

--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -18,4 +18,12 @@ export class PedidosService {
     let {idRestaurante} = this.user.getUser()
     return this.http.get<any[]>(`${this.api_url}/pedidos/restaurante/${idRestaurante}`)
   }
+
+  atualizar(item, comanda):Observable<any>{ 
+    return this.http.put<any>(`${this.api_url}/pedidos/${item.idPedido}`, {status: "preparando", idComanda: comanda.idComanda})
+  }
+
+  finalizar(item, comanda):Observable<any>{ 
+    return this.http.put<any>(`${this.api_url}/pedidos/${item.idPedido}`, {status: "finalizado", idComanda: comanda.idComanda})
+  }
 }
